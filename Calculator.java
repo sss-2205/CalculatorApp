563144341ddda99a28b3b29cafbb176a2e6692e1
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2023-09-09 05:06:16 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,15 @@
-public class Calculator{
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if (args.length != 2) {
+            System.out.println("Usage: Calculator <num1> <num2>");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int result = num1 + num2;
+
+        System.out.println("Result: " + num1 + " + " + num2 + " = " + result);
     }
 }
