0201b5ed8152d86b839843e18f4e23e6b4ecb7ff
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2023-09-09 04:44:29 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,5 @@
-public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public double divide(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero.");
-        }
-        return (double) a / b;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
